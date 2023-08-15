17275117731696b17c874bef3480b28aed226e21
Naborisk <github@naborisk.com>
2023-08-16 02:08:22 +0900

    WIP only for prod

--- src/App.svelte
+++ src/App.svelte
@@ -28,11 +28,14 @@
   }, 1000)
 </script>
 
-<div class="h-screen w-screen absolute backdrop-blur">
-  <div class="flex h-screen">
-    <div class="mx-auto my-auto text-5xl text-white">WIP</div>
+<!-- WIP overlay, only visible in production -->
+{#if import.meta.env.PROD}
+  <div class="h-screen w-screen absolute backdrop-blur">
+    <div class="flex h-screen">
+      <div class="mx-auto my-auto text-5xl text-white">WIP</div>
+    </div>
   </div>
-</div>
+{/if}
 
 <main
   class="bg-sky-900 text-white h-screen flex flex-col items-center justify-center"
